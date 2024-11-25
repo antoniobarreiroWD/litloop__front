bac88538bbaf7fc3962454fd6c4d398c30ff658e
Antonio Barreiro <148621332+antoniobarreiroWD@users.noreply.github.com>
2024-11-25 21:22:05 +0100

    Update HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import BookList from "../../components/BookList/BookList";
 import WelcomeBanner from "../../components/WelcomeBanner/WelcomeBanner";
 
 const HomePage = () => {
-  const [isBannerVisible, setIsBannerVisible] = useState(true);
+  const [isBannerVisible, setIsBannerVisible] = useState(false);
+
+  useEffect(() => {
+    
+    const hasSeenBanner = localStorage.getItem("hasSeenWelcomeBanner");
+    if (!hasSeenBanner) {
+      setIsBannerVisible(true);
+      localStorage.setItem("hasSeenWelcomeBanner", "true");
+    }
+  }, []);
 
   const hideBanner = () => {
     setIsBannerVisible(false);
@@ -12,12 +21,12 @@ const HomePage = () => {
 
   return (
     <div className="relative">
-      
+     
       {isBannerVisible && (
         <WelcomeBanner onHide={hideBanner} isVisible={isBannerVisible} />
       )}
 
-      
+    
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: isBannerVisible ? 0.5 : 1 }}
